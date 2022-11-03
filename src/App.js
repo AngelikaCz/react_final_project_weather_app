10b61632fa6ad7763ce22e3065b218a94c9b6b0e
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-11-03 19:40:12 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
     <div className="App">
       Hello Weather
       <Weather />
+      <footer>
+        This projects was coded by Angelika Czerwińska and it is open-sourced on{" "}
+        <a
+          href="https://github.com/AngelikaCz/react_final_project_weather_app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
     </div>
   );
 }
