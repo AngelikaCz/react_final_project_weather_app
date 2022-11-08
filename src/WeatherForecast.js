a5f272f950242f13a966ef1af49f8488f5511809
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-11-08 20:06:23 +0100

    Moved forecast to another component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDaily from "./WeatherForecastDaily";
 
 export default function WeatherForecast(props) {
   const [ready, setReady] = useState(false);
@@ -12,76 +13,7 @@ export default function WeatherForecast(props) {
   }
 
   if (ready) {
-    return (
-      <div className="ForecastContainer">
-        <ul className="NextDay" id="NextDay">
-          <li className="Day">Mon</li>
-          <li>
-            {" "}
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt={props.data.description}
-            ></img>
-          </li>
-          <li>12°</li>
-        </ul>
-        <ul className="NextDay" id="NextDay">
-          <li className="Day">Tue</li>
-          <li>
-            {" "}
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="cloudy icon"
-            ></img>
-          </li>
-          <li>12°</li>
-        </ul>
-        <ul className="NextDay" id="NextDay">
-          <li className="Day">Wed</li>
-          <li>
-            {" "}
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="cloudy icon"
-            ></img>
-          </li>
-          <li>12°</li>
-        </ul>
-        <ul className="NextDay" id="NextDay">
-          <li className="Day">Thu</li>
-          <li>
-            {" "}
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="cloudy icon"
-            ></img>
-          </li>
-          <li>12°</li>
-        </ul>
-        <ul className="NextDay" id="NextDay">
-          <li className="Day">Fri</li>
-          <li>
-            {" "}
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="cloudy icon"
-            ></img>
-          </li>
-          <li>12°</li>
-        </ul>
-        <ul className="NextDay" id="NextDay">
-          <li className="Day">Sat</li>
-          <li>
-            {" "}
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="cloudy icon"
-            ></img>
-          </li>
-          <li>12°</li>
-        </ul>{" "}
-      </div>
-    );
+    return <WeatherForecastDaily data={forecast[0]} />;
   } else {
   }
   let lat = props.coordinates.lat;
---
+++ src/WeatherForecastDaily.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function WeatherForecastDaily(props) {
+  function forecastTemperature() {
+    let temperature = Math.round(props.data.main.temp);
+    return `${temperature}°`;
+  }
+
+  return (
+    <div className="ForecastContainer">
+      <ul className="NextDay" id="NextDay">
+        <li className="Day">Mon</li>
+        <li>
+          {" "}
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="{props.data.description}"
+          ></img>
+        </li>
+        <li>{forecastTemperature()}</li>
+      </ul>
+      <ul className="NextDay" id="NextDay">
+        <li className="Day">Tue</li>
+        <li>
+          {" "}
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="cloudy icon"
+          ></img>
+        </li>
+        <li>12°</li>
+      </ul>
+      <ul className="NextDay" id="NextDay">
+        <li className="Day">Wed</li>
+        <li>
+          {" "}
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="cloudy icon"
+          ></img>
+        </li>
+        <li>12°</li>
+      </ul>
+      <ul className="NextDay" id="NextDay">
+        <li className="Day">Thu</li>
+        <li>
+          {" "}
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="cloudy icon"
+          ></img>
+        </li>
+        <li>12°</li>
+      </ul>
+      <ul className="NextDay" id="NextDay">
+        <li className="Day">Fri</li>
+        <li>
+          {" "}
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="cloudy icon"
+          ></img>
+        </li>
+        <li>12°</li>
+      </ul>
+      <ul className="NextDay" id="NextDay">
+        <li className="Day">Sat</li>
+        <li>
+          {" "}
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="cloudy icon"
+          ></img>
+        </li>
+        <li>12°</li>
+      </ul>{" "}
+    </div>
+  );
+}
