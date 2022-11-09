c13fc09a8d269cdc8fb715f6fd9b2b4ac9329087
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-11-09 18:13:56 +0100

    Connected icon for forecast with API

--- src/WeatherForecastDaily.js
+++ src/WeatherForecastDaily.js
@@ -16,74 +16,21 @@ export default function WeatherForecastDaily(props) {
     return days[day];
   }
 
+  function iconUrl() {
+    let URL = `http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`;
+    return `${URL}`;
+  }
+
   return (
     <div className="ForecastContainer">
       <ul className="NextDay" id="NextDay">
         <li className="Day">{day()}</li>
         <li>
           {" "}
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="{props.data.description}"
-          ></img>
+          <img src={iconUrl()} alt={props.data.weather[0].description}></img>
         </li>
         <li>{forecastTemperature()}</li>
       </ul>
-      <ul className="NextDay" id="NextDay">
-        <li className="Day">Tue</li>
-        <li>
-          {" "}
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="cloudy icon"
-          ></img>
-        </li>
-        <li>12°</li>
-      </ul>
-      <ul className="NextDay" id="NextDay">
-        <li className="Day">Wed</li>
-        <li>
-          {" "}
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="cloudy icon"
-          ></img>
-        </li>
-        <li>12°</li>
-      </ul>
-      <ul className="NextDay" id="NextDay">
-        <li className="Day">Thu</li>
-        <li>
-          {" "}
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="cloudy icon"
-          ></img>
-        </li>
-        <li>12°</li>
-      </ul>
-      <ul className="NextDay" id="NextDay">
-        <li className="Day">Fri</li>
-        <li>
-          {" "}
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="cloudy icon"
-          ></img>
-        </li>
-        <li>12°</li>
-      </ul>
-      <ul className="NextDay" id="NextDay">
-        <li className="Day">Sat</li>
-        <li>
-          {" "}
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="cloudy icon"
-          ></img>
-        </li>
-        <li>12°</li>
-      </ul>{" "}
     </div>
   );
 }
