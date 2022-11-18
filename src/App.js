ccfa2e62eab4fa71c62bf2525b566987f386d99e
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-11-18 19:14:43 +0100

    Added LinkedIn link in footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ function App() {
     <div className="App">
       <Weather defaultCity="Warsaw" />
       <footer>
-        This projects was coded by Angelika Czerwińska and it is open-sourced on{" "}
+        This projects was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/angelika-czerwinska/"
+          target="_blank"
+          rel="noreferrer"
+          className="linkedinLink"
+          title="LinkedIn Link"
+        >
+          {" "}
+          Angelika Czerwińska
+        </a>{" "}
+        and it is open-sourced on{" "}
         <a
           href="https://github.com/AngelikaCz/react_final_project_weather_app"
           target="_blank"
